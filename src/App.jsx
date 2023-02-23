1cd6658b127304cb2a8df7f0646a5e3d30027169
Tatiana <ganzhinovat@yandex.ru>
2023-02-23 23:33:46 +0300

    feat: implement Store and CategorySlice via RTK, add dynamic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
 import Catalog from "./components/Catalog/Catalog";
-import Footer from "./components/Footer/Footer.jsx";
+import Footer from "./components/Footer/Footer";
+import { Provider } from "react-redux";
+import store from "./store/index.js";
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <Header />
       <main>
         <Navigation />
         <Catalog />
       </main>
       <Footer />
-    </>
+    </Provider>
   )
 };
 
