8eea9d0af86aa0a51962ec2a4564e03d54b18899
Tatiana <ganzhinovat@yandex.ru>
2023-02-27 13:24:59 +0300

    refactor: rewrite extraReducers according to modern syntax

--- src/store/category/categorySlice.js
+++ src/store/category/categorySlice.js
@@ -22,17 +22,28 @@ const categorySlice = createSlice({
       state.activeCategory = action.payload.indexCategory;
     },
   },
-  extraReducers: {
-    [categoryRequestAsync.pending.type]: (state) => {
-      state.error = '';
-    },
-    [categoryRequestAsync.fulfilled.type]: (state, action) => {
-      state.error = '';
-      state.category = action.payload;
-    },
-    [categoryRequestAsync.rejected.type]: (state, action) => {
-      state.error = action.payload;
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(categoryRequestAsync.pending, (state) => {
+        state.error = '';
+      })
+      .addCase(categoryRequestAsync.fulfilled, (state, action) => {
+        state.error = '';
+        state.category = action.payload;
+      })
+      .addCase(categoryRequestAsync.rejected, (state, action) => {
+        state.error = action.error;
+      })
+    // [categoryRequestAsync.pending.type]: (state) => {
+    //   state.error = '';
+    // },
+    // [categoryRequestAsync.fulfilled.type]: (state, action) => {
+    //   state.error = '';
+    //   state.category = action.payload;
+    // },
+    // [categoryRequestAsync.rejected.type]: (state, action) => {
+    //   state.error = action.error;
+    // },
   }
 });
 
