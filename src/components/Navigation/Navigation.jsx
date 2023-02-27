d581dcd8b585090dec9dc9a2ced7b98daa7f7726
Tatiana <ganzhinovat@yandex.ru>
2023-02-27 13:16:28 +0300

    feat: get data from API

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,11 +2,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import Container from '../Container/Container';
 import style from './Navigation.module.css';
 import classNames from 'classnames';
-import { changeCategory } from '../../store/category/categorySlice';
+import { categoryRequestAsync, changeCategory } from '../../store/category/categorySlice';
+import { useEffect } from 'react';
+import { API_URL } from '../../const';
 
 const Navigation = () => {
   const { category, activeCategory } = useSelector((state) => state.category);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(categoryRequestAsync('max'));
+  }, [])
 
   return (
     <nav className={style.navigation}>
@@ -14,12 +20,12 @@ const Navigation = () => {
         <div className={style.container}>
             <ul className={style.list}>
               {category.map((item, i) => (
-                <li className={style.item} key={i}>
+                <li className={style.item} key={item.title}>
                   <button 
                     className={classNames(
                       style.button,
                       activeCategory === i ? style.button_active : '')}
-                      style={{ backgroundImage: `url(.${item.image})`}}
+                      style={{ backgroundImage: `url(${API_URL}/${item.image})`}}
                       onClick={() => dispatch(changeCategory({ indexCategory: i }))}
                       >
                       {item.rus}
--- src/store/category/categorySlice.js
+++ src/store/category/categorySlice.js
@@ -1,20 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { API_URL, POSTFIX } from "../../const";
 
 const initialState = {
-  category: [
-    { title: 'burger', rus: 'Бургеры', image: '/img/burger.png' },
-    { title: 'snack', rus: 'Закуски', image: '/img/snack.png' },
-    { title: 'hot-dog', rus: 'Хот-доги', image: '/img/hot-dog.png' },
-    { title: 'combo', rus: 'Комбо', image: '/img/combo.png' },
-    { title: 'shawarma', rus: 'Шаурма', image: '/img/shawarma.png' },
-    { title: 'pizza', rus: 'Пицца', image: '/img/pizza.png' },
-    { title: 'wok', rus: 'Вок', image: '/img/wok.png' },
-    { title: 'dessert', rus: 'Десерты', image: '/img/dessert.png' },
-    { title: 'sauce', rus: 'Соусы', image: '/img/sauce.png' },
-  ],
+  category: [],
   error: '',
   activeCategory: 0,
 };
+
+export const categoryRequestAsync = createAsyncThunk(
+  'category/fetch', async () => {
+    const response = await fetch(`${API_URL}${POSTFIX}/category`);
+    const dataCategory = await response.json();
+    return dataCategory;
+})
 
 const categorySlice = createSlice({
   name: 'category',
@@ -22,6 +20,18 @@ const categorySlice = createSlice({
   reducers: {
     changeCategory(state, action) {
       state.activeCategory = action.payload.indexCategory;
+    },
+  },
+  extraReducers: {
+    [categoryRequestAsync.pending.type]: (state) => {
+      state.error = '';
+    },
+    [categoryRequestAsync.fulfilled.type]: (state, action) => {
+      state.error = '';
+      state.category = action.payload;
+    },
+    [categoryRequestAsync.rejected.type]: (state, action) => {
+      state.error = action.payload;
     },
   }
 });
