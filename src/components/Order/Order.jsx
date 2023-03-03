d267c6f88fb238bb691953fde6fffecdd674a5c0
Tatiana <ganzhinovat@yandex.ru>
2023-03-03 17:59:26 +0300

    refactor: open cart by clicking on mobile version

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,22 +1,24 @@
 import style from  "./Order.module.css";
 import OrderGoods from "../OrderGoods/OrderGoods";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { orderRequestAsync } from "../../store/order/orderSlice";
 import { openModal } from "../../store/modalDelivery/modalDeliverySlice";
+import classNames from "classnames";
 
 const Order = () => {
   const { totalPrice, totalCount, orderGoods, orderList } = useSelector(state => state.order);
   const dispatch = useDispatch();
+  const [openOrder, setOpenOrder] = useState(false);
 
   useEffect(() => {
     dispatch(orderRequestAsync());
   }, [orderList.length]);
 
   return (
-    <div className={style.order}>
+    <div className={classNames(style.order, openOrder ? style.order_open : 'order_open') }>
       <section className={style.wrapper}>
-        <div className={style.header} tabIndex="0" role="button">
+        <div className={style.header} tabIndex="0" role="button" onClick={() => {setOpenOrder(!openOrder)}}>
           <h2 className={style.title}>Корзина</h2>
 
           <span className={style.count}>{totalCount}</span>
