64636b31bba23466baac457473f14f4b1180259c
Tatiana <ganzhinovat@yandex.ru>
2023-03-06 15:50:40 +0300

    refactor: change API link because of deploying Data-server to Glitch

--- src/const.js
+++ src/const.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://localhost:3024';
+//export const API_URL = 'http://localhost:3024';
+export const API_URL = 'https://immediate-gainful-aluminum.glitch.me';
 export const POSTFIX = '/api/product';
